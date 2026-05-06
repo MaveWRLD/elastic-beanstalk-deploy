858a25f37be12a1847cfa89723233f009f935d38
Jacob Quaye <kofimave@gmail.com>
2026-05-06 11:24:30 +0000

    feat: Add Thymeleaf template and HomeController for homepage rendering

---
+++ simple_application/src/main/java/org/mave/simple_application/HomeController.java
@@ -0,0 +1,13 @@
+package org.mave.simple_application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
